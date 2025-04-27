ae8246ce89ef17f5f7907fbff2a874a6d52c12b1
t33y <omotayoolarewaju@gmail.com>
2025-04-27 19:51:29 +0100

    includes dist folder

---
+++ dist/esm/definitions.js
@@ -0,0 +1,8 @@
+export var SdpType;
+(function (SdpType) {
+    SdpType["offer"] = "offer";
+    SdpType["prAnswer"] = "prAnswer";
+    SdpType["answer"] = "answer";
+    SdpType["rollback"] = "rollback";
+})(SdpType || (SdpType = {}));
+//# sourceMappingURL=definitions.js.map
---
+++ dist/plugin.js
@@ -0,0 +1,31 @@
+var capacitorSwiftFlutterCallkitIncomingPlugin = (function (exports, core) {
+    'use strict';
+
+    exports.SdpType = void 0;
+    (function (SdpType) {
+        SdpType["offer"] = "offer";
+        SdpType["prAnswer"] = "prAnswer";
+        SdpType["answer"] = "answer";
+        SdpType["rollback"] = "rollback";
+    })(exports.SdpType || (exports.SdpType = {}));
+
+    const FlutterCallkitIncoming = core.registerPlugin('FlutterCallkitIncomingWebrtc', {
+        web: () => Promise.resolve().then(function () { return web; }).then(m => new m.FlutterCallkitIncomingWeb()),
+    });
+
+    class FlutterCallkitIncomingWeb extends core.WebPlugin {
+    }
+
+    var web = /*#__PURE__*/Object.freeze({
+        __proto__: null,
+        FlutterCallkitIncomingWeb: FlutterCallkitIncomingWeb
+    });
+
+    exports.FlutterCallkitIncoming = FlutterCallkitIncoming;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+    return exports;
+
+})({}, capacitorExports);
+//# sourceMappingURL=plugin.js.map
